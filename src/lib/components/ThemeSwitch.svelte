1fd382719ffae83b37e0db40cd13a92f647914fa
Katie <5213160+KatieLG@users.noreply.github.com>
2025-11-12 15:02:34 +0000

    fix: bug with light/dark mode switching

--- src/lib/components/ThemeSwitch.svelte
+++ src/lib/components/ThemeSwitch.svelte
@@ -3,22 +3,38 @@
   import Sun from '$lib/icons/Sun.svelte';
   import Moon from '$lib/icons/Moon.svelte';
 
-  let darkMode = $state(true);
+  // By default the app uses the system preference for theme
+  // this needs to provide a toggle to light mode if the default is dark, else a toggle to dark
+
+  let systemDefaultIsDark = $state(false);
+  let toggleTheme = $state(false);
+
+  const getSystemIsDark = () => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
 
   onMount(() => {
-    darkMode = localStorage.getItem('darkMode') === 'true';
+    systemDefaultIsDark = getSystemIsDark();
+    toggleTheme = localStorage.getItem('toggleTheme') === 'true';
+    console.log(toggleTheme)
   });
 
-  $effect(() => {
-    localStorage.setItem('darkMode', darkMode ? 'true' : 'false');
-  });
+  const savePreference = () => {
+    localStorage.setItem('toggleTheme', toggleTheme ? 'true' : 'false');
+  };
 </script>
 
 <div class="fixed bottom-6 right-6 flex flex-col gap-3 z-50">
   <label class="swap swap-rotate">
-    <input type="checkbox" class="theme-controller" value="light" bind:checked={darkMode} />
+    <input
+      type="checkbox"
+      class="theme-controller"
+      value={systemDefaultIsDark ? 'light' : 'dark'}
+      bind:checked={toggleTheme}
+      onchange={savePreference}
+    />
 
-    <Sun classes="w-10 h-10" />
-    <Moon classes="w-10 h-10" />
+    <Sun classes="w-10 h-10 {systemDefaultIsDark ? 'swap-on' : 'swap-off'}" />
+    <Moon classes="w-10 h-10 {systemDefaultIsDark ? 'swap-off' : 'swap-on'}" />
   </label>
 </div>
--- src/lib/icons/Moon.svelte
+++ src/lib/icons/Moon.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <svg
-  class={`swap-off fill-current ${classes}`}
+  class={`fill-current ${classes}`}
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 24 24"
 >
