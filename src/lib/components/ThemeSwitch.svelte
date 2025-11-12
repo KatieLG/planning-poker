5654931cf37bfc35c28cf9793106fb1d66010135
Katie <5213160+KatieLG@users.noreply.github.com>
2025-11-12 18:54:35 +0000

    fix: bug with swapping theme

--- src/lib/components/ThemeSwitch.svelte
+++ src/lib/components/ThemeSwitch.svelte
@@ -9,20 +9,35 @@
   let systemDefaultIsDark = $state(false);
   let toggleTheme = $state(false);
 
-  const getSystemIsDark = () => {
-    return window.matchMedia('(prefers-color-scheme: dark)').matches;
-  }
-
   onMount(() => {
-    systemDefaultIsDark = getSystemIsDark();
+    systemDefaultIsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     toggleTheme = localStorage.getItem('toggleTheme') === 'true';
-    console.log(toggleTheme)
+    console.log(toggleTheme);
   });
 
   const savePreference = () => {
+    const theme =
+      (systemDefaultIsDark && toggleTheme) || (!systemDefaultIsDark && !toggleTheme)
+        ? 'light'
+        : 'dark';
     localStorage.setItem('toggleTheme', toggleTheme ? 'true' : 'false');
+    document.documentElement.setAttribute('data-theme', theme);
   };
 </script>
+
+<svelte:head>
+  <script lang="ts">
+    // set the theme before page render to stop a flash of the wrong theme
+    (function () {
+      const toggleTheme = localStorage.getItem('toggleTheme') === 'true';
+      const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+      const theme = (isDark && toggleTheme) || (!isDark && !toggleTheme) ? 'light' : 'dark';
+
+      document.documentElement.setAttribute('data-theme', theme);
+    })();
+  </script>
+</svelte:head>
 
 <div class="fixed bottom-6 right-6 flex flex-col gap-3 z-50">
   <label class="swap swap-rotate">
