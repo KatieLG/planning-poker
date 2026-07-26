a5de8507e2a39b72a01bb177923213500b611781
Katie <5213160+KatieLG@users.noreply.github.com>
2026-07-26 18:13:01 +0100

    feat: swap to using the better looking target svg for throwing

--- src/lib/components/ThrowButton.svelte
+++ src/lib/components/ThrowButton.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Poke from '$lib/icons/Poke.svelte';
+  import Target from '$lib/icons/Target.svelte';
 
   type Props = {
     onThrow: () => void;
@@ -9,9 +9,9 @@
 </script>
 
 <button
-  class="opacity-60 transition-transform duration-200 hover:scale-125 hover:rotate-12 hover:opacity-100 active:scale-110 active:rotate-12"
+  class="opacity-60 transition-transform duration-200 hover:scale-125 hover:opacity-100 active:scale-110"
   title="Throw emoji"
   onclick={onThrow}
 >
-  <Poke classes="w-5 h-5" />
+  <Target classes="w-5 h-5" />
 </button>
